f6c74ad5b329d56b6031914cd95e3ea4dc088736
steven18 <2602031200@qq.com>
2018-05-12 11:02:54 +0800

    add move image

--- app/src/main/java/videorawtest/example/com/videorawtest/GestureZoomActivityExActivity.java
+++ app/src/main/java/videorawtest/example/com/videorawtest/GestureZoomActivityExActivity.java
@@ -25,6 +25,11 @@ public class GestureZoomActivityExActivity extends AppCompatActivity {
     private DecimalFormat df = new DecimalFormat("0.00");//格式化float
     private float lastScale = 1;//记录上次的缩放比例，下次缩放时是在此基础上进行的
 
+    private float fLastX = 0.0f;
+    private float fLastY = 0.0f;
+
+    private Boolean bScale = false;
+    private int nMode = -1;
 
     /*使图片居中*/
     private void center() {
@@ -54,9 +59,9 @@ public class GestureZoomActivityExActivity extends AppCompatActivity {
 
         //计算Y到中心的距离
 
-        deltaY = (mScreenHeight - height) / 2  - actionBarHeight-rect.top;
+        deltaY = (mScreenHeight - height) / 2 - actionBarHeight - rect.top;
         //计算X到中心的距离
-        deltaX = (mScreenWidth - width) / 2-rect.left;
+        deltaX = (mScreenWidth - width) / 2 - rect.left;
 
         matrix.postTranslate(deltaX, deltaY);
     }
@@ -78,6 +83,8 @@ public class GestureZoomActivityExActivity extends AppCompatActivity {
         mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.SimpleOnScaleGestureListener() {
             @Override
             public boolean onScale(ScaleGestureDetector detector) {
+                bScale = true;
+                nMode = -1;
                 float scale = detector.getScaleFactor(); //缩放因子，两指靠拢时小于1
                 float x = detector.getFocusX(), y = detector.getFocusY();//中心点坐标
                 Log.i("bqt", "缩放手势  onScale，" + df.format(scale) + "-" + df.format(x) + "-" + df.format(y));
@@ -89,12 +96,14 @@ public class GestureZoomActivityExActivity extends AppCompatActivity {
 
             @Override
             public boolean onScaleBegin(ScaleGestureDetector detector) {
+                bScale = true;
                 Log.i("bqt", "缩放手势  onScaleBegin，" + df.format(detector.getScaleFactor()));//始终是1
                 return super.onScaleBegin(detector);
             }
 
             @Override
             public void onScaleEnd(ScaleGestureDetector detector) {
+                bScale = false;
                 super.onScaleEnd(detector);
                 lastScale *= detector.getScaleFactor();
                 Log.i("bqt", "缩放手势  onScaleEnd，" + df.format(detector.getScaleFactor()));
@@ -104,6 +113,42 @@ public class GestureZoomActivityExActivity extends AppCompatActivity {
         mImageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                if (bScale == false && event.getPointerCount() == 1) {
+                    int nAction = event.getAction();
+                    nAction = nAction & MotionEvent.ACTION_MASK;
+                    switch (nAction) {
+                        case MotionEvent.ACTION_DOWN:
+                            nMode = 1;
+                            fLastX = event.getX();
+                            fLastY = event.getY();
+                        case MotionEvent.ACTION_MOVE: {
+
+                            if (nMode != 1)
+                                break;
+                            float distanceX = event.getX() - fLastX;
+                            float distanceY = event.getY() - fLastY;
+                            fLastX = event.getX();
+                            fLastY = event.getY();
+
+                            if (Math.abs(distanceX) > 50 || Math.abs(distanceY) > 50)
+                                break;
+
+                            Log.i("bqt", "平移 distanceX=" + distanceX + " distanceY" + distanceY);
+                            matrix.postTranslate(distanceX, distanceY);
+                            mImageView.setImageMatrix(matrix);
+                        }
+                        break;
+                        case MotionEvent.ACTION_POINTER_UP:
+                            nMode = -1;
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            nMode = -1;
+                            break;
+                        default:
+                            break;
+                    }
+                    return true;
+                }
                 return mScaleGestureDetector.onTouchEvent(event);
             }
         });
